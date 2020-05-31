b897131a2384b38df6f4bcac374bd63d0b7149c2
Jim Homme <jjhomme1028@gmail.com>
2020-05-31 07:39:49 -0400

    Farkle project, variables, 004a

--- farkle.py
+++ farkle.py
@@ -1,26 +1,21 @@
 # Farkle.py
-# Version 003a
-# Stepping back and getting ready for more in the next program
-# Print more strings, show some math, show if the game is over, true/false.
-# Print an ending message
- 
-print ("Hi there.") # A string in double quotes.
-print ('How are you?') # A string in single quotes.
-print("Here is some math for you.")
-print ("5+4 is ", 5+4)
-print ("5-4 is ", 5-4)
-print ("5*4 is ", 5*4)
-print ("5/4 is ", 5/4)
-print ("5%4 is ", 5%4)
-print ("Let's see something about true and false.")
-print ("The game goes up to 10000.")
-print("Is the game over yet? ", 10000 == 10000)
-print ("Is 10000 unequal to 9000? ", 10000 != 9000)
-print("is 9000 less than 10000? ", 9000 < 10000)
-print ("You have 10000. I have 9000. Did you win? ", 10000 > 9000)
-print("Is 9000 less than or equal to 10000? ", 9000 <= 10000)
-print("is 10000 greater than or equal to 10000? ", 10000 >= 10000)
-print("I have 10000. You have 9000. Did I lose?", 10000 < 9000)
-print(" Is 9000 greater than 10000?",9000 > 10000)
-print("Does 9000 equal 10000? ", 9000 == 10000)
-print ("That's it!")
+# Version 004a
+# Variables and printing
+
+# Assign Variables
+
+d1 = 1
+d2 = 2
+d3 = 3
+d4 = 4
+d5 = 5
+d6 = 6
+
+# Print what is in the variables
+
+print (d1)
+print(d2)
+print(d3)
+print(d4)
+print(d5)
+print(d6)
